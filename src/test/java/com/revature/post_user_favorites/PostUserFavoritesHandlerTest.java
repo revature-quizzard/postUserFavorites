e5a780d7af9ec358a03424fb1d44a5dde382ce79
heraldOfMechanus <jccallahan11@gmail.com>
2021-09-28 17:03:33 -0500

    Added a little padding for the test class

--- src/test/java/com/revature/post_user_favorites/PostUserFavoritesHandlerTest.java
+++ src/test/java/com/revature/post_user_favorites/PostUserFavoritesHandlerTest.java
@@ -1,18 +1,49 @@
 package com.revature.post_user_favorites;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.revature.post_user_favorites.stubs.TestLogger;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class PostUserFavoritesHandlerTest {
 
-    @org.junit.jupiter.api.BeforeEach
+    static TestLogger testLogger;
+    private Gson mapper = new GsonBuilder().setPrettyPrinting().create();
+
+    PostUserFavoritesHandler sut;
+    Context mockContext;
+    UserRepository mockUserRepo;
+
+    @BeforeAll
+    static void beforeAll() { testLogger = new TestLogger(); }
+
+    @BeforeEach
     void setUp() {
+        mockContext = mock(Context.class);
+        mockUserRepo = mock(UserRepository.class);
+
+        sut = new PostUserFavoritesHandler(mockUserRepo);
+
+        when(mockContext.getLogger()).thenReturn(testLogger);
     }
 
-    @org.junit.jupiter.api.AfterEach
+    @AfterEach
     void tearDown() {
+        sut = null;
+        mockContext = null;
+        mockUserRepo = null;
     }
 
-    @org.junit.jupiter.api.Test
+    @AfterAll
+    void afterAll() {
+        testLogger.close();
+    }
+
+    @Test
     void handleRequest() {
     }
 }

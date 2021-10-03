4010c9bff31db1953698173f9b8c3659c9784ead
hpuf <csmcdn@gmail.com>
2021-10-02 23:56:15 -0500

    Incrementing favorite count on setdoc and set for resource persistance

---
+++ src/main/java/com/revature/post_user_favorites/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,2 @@
+package com.revature.post_user_favorites.exceptions;public class ResourceNotFoundException {
+}
---
+++ src/main/java/com/revature/post_user_favorites/repositories/SetRepository.java
@@ -0,0 +1,2 @@
+package com.revature.post_user_favorites.repositories;public class SetRepository {
+}
